57a1166cade34e79886c4cffabcd43a082cedfc2
louismeunier <louismeunier123@gmail.com>
2021-10-14 10:44:10 -0400

    change logic of workflow

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ const authorizedFunction = require("./auth");
 const getAllEmails = require("./get_emails");
 const parseEmails = require("./parse_emails");
 
+require('dotenv').config();
+
 authorizedFunction()
     .then(auth => getAllEmails(auth)
         .then(urls => parseEmails(urls))
