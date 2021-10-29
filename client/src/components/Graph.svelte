3d180dc020b2d24cd5dd3250d1ad0263c0697258
louismeunier <louismeunier123@gmail.com>
2021-10-28 21:07:46 -0400

    Update Graph to work with new data

--- client/src/components/Graph.svelte
+++ client/src/components/Graph.svelte
@@ -2,12 +2,12 @@
     import Chart from 'svelte-frappe-charts';
     import * as raw_data from "../data.json";
     
-    const topColleges = Object.keys(raw_data.byCollege).sort((a, b) => raw_data.byCollege[b] - raw_data.byCollege[a]);
+    const topColleges = Object.keys(raw_data.byCollege).sort((a, b) => raw_data.byCollege[b].value - raw_data.byCollege[a].value);
     let data = {
       labels: topColleges.slice(0,5),
       datasets: [
         {
-          values: topColleges.map(college => raw_data.byCollege[college]),
+          values: topColleges.map(college => raw_data.byCollege[college].value),
         }
       ]
     };
